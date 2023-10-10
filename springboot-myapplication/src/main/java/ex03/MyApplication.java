476eb6ac32fed48bfb07c725dea9a905e2903c5a
yyhjin <wendy0301@naver.com>
2023-10-10 14:17:20 +0900

    test

---
+++ springboot-myapplication/src/main/java/ex03/MyApplication.java
@@ -0,0 +1,22 @@
+package ex03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import ex03.component.MyComponent;
+
+@SpringBootConfiguration
+public class MyApplication {
+	
+	@Bean
+	public MyComponent myComponent() {
+		return new MyComponent();
+	}
+	
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext ac = SpringApplication.run(MyApplication.class, args)) {};
+	}
+	
+}
